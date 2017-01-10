959c0c65313030060f8a2ed9f7a2b303025f9631
Elliot Cheung <elliotccheung@gmail.com>
2017-01-09 18:07:28 -0600

    Added firebase

--- index.android.js
+++ index.android.js
@@ -5,12 +5,23 @@
  */
 
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View
 } from 'react-native';
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyDU3qympK0n7Ts6ZsgRB2CqE39fUu_W2I8",
+  authDomain: "stately-shower.firebaseapp.com",
+  databaseURL: "https://stately-shower.firebaseio.com",
+  storageBucket: "stately-shower.appspot.com",
+  messagingSenderId: "507309554511"
+};
+const firebaseApp = firebase.initializeApp(config);
 
 export default class statelyshowermobile extends Component {
   render() {
